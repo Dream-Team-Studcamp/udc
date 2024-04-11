b186bcc8bc5b8179e27d3b2a09db8840aef43339
root <root@studcamp2.ru-central1.internal>
2024-04-11 18:27:36 +0000

    Refactored  bit

--- udc-app-backend/app/udcs.py
+++ udc-app-backend/app/udcs.py
@@ -14,7 +14,7 @@ from pathlib import Path
 nltk.download("stopwords")
 nltk.download("punkt")
 
-MODELS_PATH: str = Path(__file__).parent.parent / 'models'
+MODELS_PATH: str = Path(__file__).parent / 'models'
 
 stop_words = set(stopwords.words('russian') + stopwords.words('english'))
 morph = MorphAnalyzer()
@@ -38,13 +38,13 @@ def clean_text(text: str) -> str:
 
 def get_udcs(text: str) -> List[Tuple[str, str]]:
     text = clean_text(text)
-    text = vect_tfidf.transform(text)
-    text = scaler.transform(text)
-    proba = log_reg.predict_proba(text)
+    array = vect_tfidf.transform([text])
+    array = scaler.transform(array)
+    proba = log_reg.predict_proba(array)
     top_5_classes = np.argsort(proba)[0][::-1][:5]
     udcs = log_reg.classes_[top_5_classes]
     result = [
-        (f"{udc}", "https://teacode.com/online/udc/{c}/{c}.html")
+        (f"{udc}", f"https://teacode.com/online/udc/{udc}/{udc}.html")
         if udc != "00" else ("00", "https://teacode.com/online/udc/00/00.html")
         for udc in udcs
     ]
